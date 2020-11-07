903a6b1ae2f345bd19648839488bb111294b89d9
Hemin Johnson <achayan1992@gmail.com>
2020-11-07 11:00:32 +0100

    feat: db connection is required only if mentioned in env

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const helloRouter = require('./routers/hello')
+const express = require("express");
+if (process.env.DB_CONNECTION) {
+  require("./db/mongoose");
+}
+const userRouter = require("./routers/user");
+const helloRouter = require("./routers/hello");
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
-app.use(express.json())
-app.use(userRouter)
-app.use(helloRouter)
+app.use(express.json());
+app.use(userRouter);
+app.use(helloRouter);
 
 app.listen(port, () => {
-  console.log('Server is up on port ' + port)
-})
+  console.log("Server is up on port " + port);
+});
