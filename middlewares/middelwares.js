481b556582d55d10c6f670d1b78d30d1caf319dd
Hemin Johnson <achayan1992@gmail.com>
2020-11-07 12:05:55 +0100

    feat: configure security

--- middlewares/middelwares.js
+++ middlewares/middelwares.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const cors = require('cors')
-const morgan = require('morgan')
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
-module.exports = [
-  express.json(),
-  cors(),
-  morgan('combined')
-]
+module.exports = [express.json(), cors(), morgan("combined"), helmet()];
