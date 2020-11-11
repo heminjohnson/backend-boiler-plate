b139fd99fad0b8985e4dd46d5331722d662c6a80
Hemin Johnson <achayan1992@gmail.com>
2020-11-11 12:25:51 +0100

    feat: separate server from index

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-const express = require("express");
-if (process.env.DB_CONNECTION) {
-  require("./db/mongoose");
-}
-const userRouter = require("./routers/user");
-const helloRouter = require("./routers/hello");
-
-const app = express();
+const app = require('./server.js')
 const port = 5000;
-
-app.use(require("./middlewares"));
-
-app.use(userRouter);
-app.use(helloRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
